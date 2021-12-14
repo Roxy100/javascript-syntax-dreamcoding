3ca2e7fcd472d51ab405daca9fe3bd7db5caae86
Roxy100 <misoamy16@gmail.com>
2021-12-14 17:11:42 +0900

    Callback Hell 예제 => Promise 식으로 바꿔보기 callback-to-promise.js

---
+++ async/callback-to-promise.js
@@ -0,0 +1,40 @@
+// Callback Hell => Promise 식으로 바꿔보기
+class UserStorage {
+  // 사용자를 로그인하는 것.
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "roxy" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
+  }
+  // 사용자 데이터를 받아서 사용자마다 admin or guest의 역할을 가져와서 서버에게 요청해서 다시 받아가지고 올 때.
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "roxy") {
+          resolve({ name: "roxy", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      }, 1000);
+    });
+  }
+}
+
+// 코딩 시작!
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role!`))
+  .catch(console.log);
